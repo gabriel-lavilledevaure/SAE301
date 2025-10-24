2264e94b91969ab473145059086c1e45b2f74d4e
Gabriel <gabriel.lavilledevaure@etu.unilim.fr>
2025-10-24 12:38:18 +0200

    Add deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "SAE301", 
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        account: resolve(__dirname, "pages/account.html"),
+        article: resolve(__dirname, "pages/article.html"),
+        createaccount: resolve(__dirname, "pages/createaccount.html"),
+        panier: resolve(__dirname, "pages/panier.html"),
+      },
+    },
+  },
 });
